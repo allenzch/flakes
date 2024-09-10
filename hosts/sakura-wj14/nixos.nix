6fa2f688a41a5090a1d4c2c663c89a09caa432d7
allenzch <hcznella@gmail.com>
2024-09-10 10:16:16 +0800

    hosts/sakura-wj14: remap keyboard

--- hosts/sakura-wj14/nixos.nix
+++ hosts/sakura-wj14/nixos.nix
@@ -102,6 +102,10 @@
       evdev:input:b*v046Dp4089*
         KEYBOARD_KEY_70039=esc
         KEYBOARD_KEY_70029=capslock
+
+      evdev:input:b*v258Ap1006*
+        KEYBOARD_KEY_70039=esc
+        KEYBOARD_KEY_70029=capslock
     '';
   };
 
