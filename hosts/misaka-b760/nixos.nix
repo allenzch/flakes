66b3ced872a0b5e8bd5960d28a3f5087025015f9
allenzch <hcznella@gmail.com>
2025-03-11 15:35:37 +0800

    hosts/misaka-b760: add keyboard remap in hwdb

--- hosts/misaka-b760/nixos.nix
+++ hosts/misaka-b760/nixos.nix
@@ -103,6 +103,10 @@
       evdev:input:b*v046Dp4089*
         KEYBOARD_KEY_70039=esc
         KEYBOARD_KEY_70029=capslock
+
+      evdev:input:b*v1A81p2039*
+        KEYBOARD_KEY_70039=esc
+        KEYBOARD_KEY_70029=capslock
     '';
   };
 
