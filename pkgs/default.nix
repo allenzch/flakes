190d0f41efad1cf8774119f352f74f81383f4e82
allenzch <hcznella@gmail.com>
2025-04-05 22:22:30 +0800

    pkgs/netns-proxy: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,5 @@
   systemd-run-app = pkgs.callPackage ./systemd-run-app { };
   texmacs = pkgs.callPackage ./texmacs { };
   canokey-udev-rules = pkgs.callPackage ./canokey-udev-rules { };
+  netns-proxy = pkgs.callPackage ./netns-proxy { };
 }
