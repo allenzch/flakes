dd2a024fc267470ea3b632e8cd49ed1b303cb399
allenzch <hcznella@gmail.com>
2025-04-11 16:50:15 +0800

    home-manager/logseq: update to support text-input-v3

--- home-manager/profiles/programs/logseq.nix
+++ home-manager/profiles/programs/logseq.nix
@@ -1,7 +1,18 @@
-{ pkgs-stable, ... }:
+{ pkgs, ... }:
+let
+  logseq-wrapped = pkgs.symlinkJoin {
+    name = "logseq";
+    paths = [ pkgs.logseq ];
+    nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+    postBuild = ''
+      wrapProgram "$out/bin/logseq" \
+        --add-flags '--wayland-text-input-version=3'
+    '';
+  };
+in
 {
   home.packages = [
-    pkgs-stable.logseq
+    logseq-wrapped
   ];
   envPersist.directories = [
     ".logseq"
