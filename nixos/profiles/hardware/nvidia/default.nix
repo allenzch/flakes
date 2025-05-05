acfa37725972046e23cd1ddf69c2a70a50ca665e
allenzch <hcznella@gmail.com>
2025-05-06 03:43:38 +0800

    hardware/nvidia: update config

--- nixos/profiles/hardware/nvidia/default.nix
+++ nixos/profiles/hardware/nvidia/default.nix
@@ -1,10 +1,19 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
+  nixpkgs.config = {
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+      "nvidia-persistenced"
+    ];
+  };
+
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.beta;
     modesetting.enable = true;
     open = false;
   };
+
   boot = {
     kernelParams = [
       "nvidia_drm.fbdev=1"
@@ -12,5 +21,6 @@
       #"nvidia.NVreg_UsePageAttributeTable=1"
     ];
   };
+
   services.xserver.videoDrivers = [ "nvidia" ];
 }
