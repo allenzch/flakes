0322c94566641fb805ffb6a6d7ae27bd375d4366
allenzch <hcznella@gmail.com>
2025-05-06 18:54:23 +0800

    users/root: cleanup

--- nixos/profiles/users/root/default.nix
+++ nixos/profiles/users/root/default.nix
@@ -1,15 +1,15 @@
-{ config, ... }:
+{ config, self, ... }:
 {
   users.users.root = {
     hashedPasswordFile = config.sops.secrets."user-password/root".path;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrBKIBvt+ktGk0wfGC6dXB3AhH/kq7lgrCNNV7l5fWj allenzch@misaka-b760"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNDUqt2SdN4i2lt5HiAOfIDxZSCgRcatL5OdXaEM2Xk allenzch@sakura-wj14"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrBKIBvt+ktGk0wfGC6dXB3AhH/kq7lgrCNNV7l5fWj"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNDUqt2SdN4i2lt5HiAOfIDxZSCgRcatL5OdXaEM2Xk"
     ];
   };
 
   sops.secrets."user-password/root" = {
     neededForUsers = true;
-    sopsFile = ../../../../secrets/local.yaml;
+    sopsFile = "${self}/secrets/local.yaml";
   };
 }
