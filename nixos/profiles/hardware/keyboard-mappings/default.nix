7a6e7d4d55164bfe9f7ebc1ba8bdfee636c012f0
allenzch <hcznella@gmail.com>
2025-05-07 18:30:24 +0800

    hardware/keyboard-mappings: init

---
+++ nixos/profiles/hardware/keyboard-mappings/default.nix
@@ -0,0 +1,14 @@
+{
+  services = {
+    udev.extraHwdb = ''
+      evdev:input:b*v046Dp4089*
+        KEYBOARD_KEY_70039=esc
+        KEYBOARD_KEY_70029=capslock
+
+      evdev:input:b*v1A81p2039*
+        KEYBOARD_KEY_70039=esc
+        KEYBOARD_KEY_70029=capslock
+
+    '';
+  };
+}
