a22c8b65c8ff7e3f99683953cddba069e0176507
allenzch <hcznella@gmail.com>
2025-05-07 18:34:16 +0800

    services/greetd: init

---
+++ nixos/profiles/services/greetd/default.nix
@@ -0,0 +1,9 @@
+{ pkgs, lib, ... }:
+{
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = "${lib.getExe pkgs.greetd.tuigreet} --cmd wayland-session";
+    };
+  };
+}
