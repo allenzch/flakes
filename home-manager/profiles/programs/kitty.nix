0419bdf4f218fa0c18409b64c7e6ed388d132e42
allenzch <hcznella@gmail.com>
2025-08-15 16:01:58 +0800

    home-manager/kitty: enable cursor trail

--- home-manager/profiles/programs/kitty.nix
+++ home-manager/profiles/programs/kitty.nix
@@ -10,6 +10,9 @@ in {
     };
     settings = mkMerge [
       {
+        cursor_trail = "1";
+        cursor_trail_decay = "0.1 0.4";
+        cursor_trail_start_threshold = "2";
         shell = ".";
         window_padding_width = "0";
         window_border_width = "0";
