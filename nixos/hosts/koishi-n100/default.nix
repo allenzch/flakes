87f31e27524bed84c04ce8a183c86bfad4fb2155
allenzch <hcznella@gmail.com>
2025-09-29 05:36:34 +0800

    hosts/koishi-n100: add openssh authorized keys

--- nixos/hosts/koishi-n100/default.nix
+++ nixos/hosts/koishi-n100/default.nix
@@ -10,6 +10,13 @@
     ])
   );
 
+  users.users.root.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrBKIBvt+ktGk0wfGC6dXB3AhH/kq7lgrCNNV7l5fWj"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNDUqt2SdN4i2lt5HiAOfIDxZSCgRcatL5OdXaEM2Xk"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfKG/KKgC6IaK4uu9zn+0wbF4XXK1pcCP/S37u6OAmJ"
+  ];
+
+
   security.polkit.enable = true;
 
   environment.systemPackages = with pkgs; [
--- nixos/profiles/users/root/default.nix
+++ nixos/profiles/users/root/default.nix
@@ -2,10 +2,6 @@
 {
   users.users.root = {
     hashedPasswordFile = config.sops.secrets."user-password/root".path;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrBKIBvt+ktGk0wfGC6dXB3AhH/kq7lgrCNNV7l5fWj"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICNDUqt2SdN4i2lt5HiAOfIDxZSCgRcatL5OdXaEM2Xk"
-    ];
   };
 
   sops.secrets."user-password/root" = {
