e6dc2fe090d94a5b57def79a3a6e8eb91dc8fe6f
allenzch <hcznella@gmail.com>
2026-08-01 23:53:06 +0800

    inputs: bump

--- flake.nix
+++ flake.nix
@@ -94,7 +94,7 @@
       flake = false;
     };
     niri-flake = {
-      url = "github:sodiboo/niri-flake/very-refactor";
+      url = "github:epireyn/niri-flake/very-refactor";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
